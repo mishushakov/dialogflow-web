e58f504403b10ec9011f78717d141fb60d2626c5
MishUshakov <mish.ushakov@gmail.com>
2017-10-27 14:07:40 +0200

    Updated service worker

--- worker.js
+++ worker.js
@@ -1,16 +1,15 @@
 importScripts('/worker-polyfill.js')
 self.addEventListener('install', function(event) {
     
-    console.log("Google Moogle worker installed")
-    event.waitUntil(caches.open('mu').then(function(cache) {
+    console.log("Dialogflow Service Worker installed")
+    event.waitUntil(caches.open('dialogflow').then(function(cache) {
         return cache.addAll([
             '/',
             '/index.html',
             'https://fonts.googleapis.com/icon?family=Material+Icons',
             'https://unpkg.com/material-components-web@0.20.0/dist/material-components-web.min.css',
-            'https://mish.io/Ushakov/images/logo/Placeholder.png',
-            '/dist/manifest_icons/web_hi_res_512.png',
-            '/dist/build.js'
+            '/dist/build.js',
+            '/dist/build.js.map'
         ])
     }))
 })
