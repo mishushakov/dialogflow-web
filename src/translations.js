ccc56f2c904ec78cd9081d785ba7475b9c3e9230
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-03-27 10:21:57 +0200

    added translations

--- src/translations.js
+++ src/translations.js
@@ -1,12 +1,36 @@
 export default {
     en: {
-        message: {
-            welcome: 'Hello, ask something to get started'
+        welcome: {
+            title: 'Hello, ask something to get started',
+            subtitle: 'You can type "Hello" for example. Or just press on the microphone to talk'
+        },
+        offline: {
+            title: 'Oh, no!',
+            subtitle: 'It looks like you are not connected to the internet, this webpage requires internet connection, to process your requests'
+        },
+        about: {
+            developedBy: 'Proudly powered by'
+        },
+        generic: {
+            inputPlaceholder: 'Ask me something...',
+            defaultSpeech: 'Go ahead, im listening...'
         }
     },
     fr: {
-        message: {
-            welcome: 'Bonjour, demandez quelque chose pour démarrer'
+        welcome: {
+            title: 'Bonjour, demandez quelque chose pour démarrer',
+            subtitle: 'Vous pouvez taper "Bonjour" par exemple, ou simplement appuyer sur le micro pour parler'
+        },
+        offline: {
+            title: 'Oups !',
+            subtitle: 'Il semble que vous n\'êtes pas connecté à Internet, cette page Web nécessite une connexion Internet, pour traiter vos demandes'
+        },
+        about: {
+            developedBy: 'Fièrement propulsé par'
+        },
+        generic: {
+            inputPlaceholder: 'Demandez-moi quelque chose...',
+            defaultSpeech: 'Allez-y, je vous écoute...'
         }
     }
 }
